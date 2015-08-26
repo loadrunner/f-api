dda5374186d2362aa35d6b1cd5fcfd179c1eed48
Danut Chereches <load.runner@weednet.ro>
2015-08-27 00:27:46 +0300

    added more fields and validators to client

--- db.js
+++ db.js
@@ -35,12 +35,21 @@ var UserSchema = mongoose.Schema({
 models.User = mongoose.model('User', UserSchema);
 
 var ClientSchema = mongoose.Schema({
-	_id     : { type : mongoose.Schema.Types.ObjectId, auto: true },
-	user_id : mongoose.Schema.Types.ObjectId,
-	name    : String,
-	cui     : String
+	_id          : { type : mongoose.Schema.Types.ObjectId, auto: true },
+	user_id      : mongoose.Schema.Types.ObjectId,
+	name         : { type : String, required : true, minlength : 2, maxlength : 100 },
+	cif          : { type : String, required : true },
+	address      : { type : String, required : false, minlength : 0, maxlength : 100 },
+	city         : { type : String, required : true, minlength : 3, maxlength : 50 },
+	county       : { type : String, required : true, minlength : 3, maxlength : 50 },
+	country      : { type : String, required : true, minlength : 3, maxlength : 50 },
+	bank_name    : { type : String, required : false, minlength : 0, maxlength : 50 },
+	bank_account : { type : String, required : false, minlength : 0, maxlength : 50 }
 });
 models.Client = mongoose.model('Client', ClientSchema);
+models.Client.schema.path('cif').validate(function (value) {
+	return /^(RO)?([0-9]{5,14})$/i.test(value);
+}, 'Invalid CIF');
 
 var ProductSchema = mongoose.Schema({
 	_id     : { type : mongoose.Schema.Types.ObjectId, auto: true },
--- resources/clients.js
+++ resources/clients.js
@@ -16,12 +16,27 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 	db.models.Client.create({
-		user_id : req.user._id,
-		name    : req.body.name,
-		cui     : req.body.cui
+		user_id      : req.user._id,
+		name         : req.body.name,
+		cif          : req.body.cif,
+		address      : req.body.address,
+		city         : req.body.city,
+		county       : req.body.county,
+		country      : req.body.country,
+		bank_name    : req.body.bank_name,
+		bank_account : req.body.bank_account
 	}, function (err, doc) {
-		if (err)
-			return next(err);
+		if (err) {
+			var result = {
+				message : err.message,
+				errors  : []
+			};
+			
+			for (var key in err.errors)
+				result.errors.push(err.errors[key].message);
+			
+			return res.status(400).json(result);
+		}
 		
 		res.json(doc);
 	});
